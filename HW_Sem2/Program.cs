16e6e1649468f8c7bcf1160bc576e49abdac8100
Anton Sopov <recorrd@yandex.ru>
2023-12-30 01:19:19 +0300

    add Task1 in folder Task1

--- HW_Sem2/Program.cs
+++ HW_Sem2/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// "&&" = "И": апельсины И яблоки (покупаем и то, и другое)
+// "||" = "ИЛИ": апельсины ИЛИ яблоки(что-то одно, либо все вместе)
+if (number % 7 == 0 && number % 23 == 0)
+{
+        Console.WriteLine($"Число: {number} кратно и 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число: {number} не кратно и 7 и 23");
+}   

2ee177fc78003e99a2377f8e5a444aa2eefe4558
Anton Sopov <recorrd@yandex.ru>
2023-12-30 22:47:23 +0300

    Add Task4 in branch Task4

--- HW_Sem2/Program.cs
+++ HW_Sem2/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.Write("Введите число: ");
+long number = (long)Convert.ToInt64(Console.ReadLine());
+int i = 18; // Максимальный размер типа Long - 1
+long number2 = 0;
+while (number > 0)
+{
+    number2 = (number % 10) * (long)Math.Pow(10, i) + number2;
+    i -- ;
+    number = number / 10;
+}
+if (number2 % 10 == 0)      // Если число короткое, то стираем нули после разворота числа.
+while (number2 % 10 == 0)
+{
+    number2 = (number2 / 10) ;
+}
+i = 0;  
+while (number2 > 0)
+{
+    number = (number2 % 10); 
+    i ++ ;
+    number2 = number2 / 10;
+    if (number2 > 0)
+    {
+     Console.Write($"{number}, ");
+    }
+    else {
+     Console.Write($"{number}");
+    }
+}

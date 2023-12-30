082a71dd36831f832835f9dc7f3366c3ee6e9828
Anton Sopov <recorrd@yandex.ru>
2023-12-30 03:08:21 +0300

    Add Task3 in branch main

--- HW_Sem2/Program.cs
+++ HW_Sem2/Program.cs
@@ -49,3 +49,32 @@
 // {
 //     Console.WriteLine($"Точка расположена в центре координат");
 // }   
+
+// // Task3
+
+// Console.Write("Введите двузначное число в промежутке между 10 и 99: ");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// // "&&" = "И": апельсины И яблоки (покупаем и то, и другое)
+// // "||" = "ИЛИ": апельсины ИЛИ яблоки(что-то одно, либо все вместе)
+
+// while (number < 10 || number > 99)
+// {
+//     Console.WriteLine($"Пожалуйста введите корректное значение");
+//     number = Convert.ToInt32(Console.ReadLine());
+// }
+// if (number >= 10 && number <= 99)
+// {
+//     int firstDigit = number / 10; // 275 / 100 = 2
+//     int secondDigit = number % 10; // 275 % 10 = 5
+//     if (firstDigit >= secondDigit)
+//     {
+//         Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit}");
+//     }
+
+// }

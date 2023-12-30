34b8838c32b8988102e0f103b7b31d8b662b5bef
Anton Sopov <recorrd@yandex.ru>
2023-12-30 22:50:40 +0300

    Add format text

--- HW_Sem2/Program.cs
+++ HW_Sem2/Program.cs
@@ -5,25 +5,26 @@ long number2 = 0;
 while (number > 0)
 {
     number2 = (number % 10) * (long)Math.Pow(10, i) + number2;
-    i -- ;
+    i--;
     number = number / 10;
 }
 if (number2 % 10 == 0)      // Если число короткое, то стираем нули после разворота числа.
-while (number2 % 10 == 0)
-{
-    number2 = (number2 / 10) ;
-}
-i = 0;  
+    while (number2 % 10 == 0)
+    {
+        number2 = (number2 / 10);
+    }
+i = 0;
 while (number2 > 0)
 {
-    number = (number2 % 10); 
-    i ++ ;
+    number = (number2 % 10);
+    i++;
     number2 = number2 / 10;
     if (number2 > 0)
     {
-     Console.Write($"{number}, ");
+        Console.Write($"{number}, ");
     }
-    else {
-     Console.Write($"{number}");
+    else
+    {
+        Console.Write($"{number}");
     }
 }
